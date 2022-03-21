34ad008f32fda4ab91be067cf25f912217aea3a6
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-03-21 15:12:03 -0400

    Added new features

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,51 +1,55 @@
+//In your project, display the current date and time using JavaScript: Tuesday 16:00
+const updateTime = () => {
+    currentTime = document.querySelector("#curren_time")
+    currentDate = new Date()
+
+    console.log(currentTime)
+    fromatedDate = currentDate.toLocaleDateString("en-us",
+        {
+            weekday: "long",
+            hour12: false,
+            hour: "2-digit",
+            minute: "2-digit"
+        })
+    currentTime.innerHTML = fromatedDate
+}
+
+updateTime()
+
+
+//Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
 const getCityName = () => {
-    city = prompt('Please enter the city Name')
+    search = document.querySelector('#search');
+    errorMessage = document.querySelector('#error_message')
+    city = document.querySelector('#location')
+    console.log(search)
+    console.log()
 
-    if(city == "") {
-        alert('That is not a vaild city name.')
-        return false
-    }
-    return city
-}
-
-const getWeather = (city) => {
-    city.trim().toLowerCase()
-    let cityInfo = {}
-    cityFound = false;
-    for(key in weather) {
-        if(key == city) {
-            cityInfo = {
-                city: key,
-                tempC: Math.round(weather[key].temp),
-                tempF: Math.round((weather[key].temp * 1.8) + 32),
-                humidity: weather[key].humidity
-            }
-            cityFound = true;
-            break;
-        } 
-    }
-    if(cityFound) {
-        return cityInfo
+    if(search.value === "") {
+        errorMessage.style.display = "inline-block"
     } else {
-        return cityFound
+        errorMessage.style.display = "none"
+        city.innerHTML = search.value;
     }
 }
+let searchForm = document.querySelector("#search_form")
+searchForm.addEventListener('submit', getCityName);
 
-const displayWeather = () => {
-    city = getCityName()
+//Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
 
-    
-    if(city != false) {
-        currentWeather = getWeather(city);
-        if(currentWeather != false) {
-            console.log(`It is currently ${currentWeather.tempC}°C (${currentWeather.tempF}°F) in Paris with a humidity of ${currentWeather.humidity}`)
-        } else {
-            alert("Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney")
-        }
+const convertTemperature = (event) => {
+    event.preventDefault()
+    currentTemp = document.querySelector('#current_temp')
+    convert = document.querySelector('#convert')
+
+    if(convert.innerHTML === '°C') {
+        currentTemp.innerHTML = Math.round((currentTemp.innerHTML * 1.8) + 32)
+        convert.innerHTML = '°F'
     } else {
-        // a function that returns itself if the user enters ""
-        displayWeather();
+        currentTemp.innerHTML = Math.round((currentTemp.innerHTML - 32) * .55)
+        convert.innerHTML = '°C'
     }
 }
 
-displayWeather()
+convert = document.querySelector('#convert')
+convert.addEventListener('click', convertTemperature)
