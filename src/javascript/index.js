bb0b50d9608da733d4aac0ac0ae2e51904693321
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-03-23 14:07:39 -0400

    Completed the project

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -36,12 +36,33 @@ const getCityName = () => {
 }
 let searchForm = document.querySelector("#search_form")
 
+const forecast = response => {
+    console.log(response.data.daily[0])
+    let daily = document.querySelectorAll('.daily')
+    for(let i = 0; i < daily.length; i++) {
+        console.log(daily[i])
+        daily[i].querySelector('.day').innerHTML = convertDay(response.data.daily[i].dt)
+        daily[i].querySelector('.day_temp .min').innerHTML = Math.round(response.data.daily[i].temp.min)
+        daily[i].querySelector('.day_temp .max').innerHTML = Math.round(response.data.daily[i].temp.max)
+        daily[i].querySelector('.day_condition').innerHTML = response.data.daily[i].weather[0].main
+        daily[i].querySelector('.day_icon').src=`https://openweathermap.org/img/wn/${response.data.daily[i].weather[0].icon}@2x.png` 
+        
+    }
+}
+
+const convertDay = (date) => {
+    let day = new Date(date * 1000)
+    day = day.toLocaleDateString('en-us' ,{weekday: "short"}).split(' ')[0]
+    return day
+}
+
+ 
+
 const currentWeather = (response) => {
     let city = document.querySelector('#location')
     let currentTemp = document.querySelector('#current_temp')
     let currentHumidity = document.querySelector('#current_humidity')
     let currentCondition = document.querySelector('#current_condition')
-    console.log(response.data)
     let currentSpeed = document.querySelector('#current_speed')
     let currentIcon = document.querySelector('.weather_icons.current_image')
     console.log("test current icon", currentIcon)
@@ -60,27 +81,49 @@ const searchByName = (event) => {
     city = getCityName();
     if(city != false) {
         getAPICity('weather', city)    
+        getAPICity('onecall', city)    
     } 
 }
 
+
+
+
+
 const getAPICity = async (type, city) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+
     if(type == 'weather') {
-        let url = `https://api.openweathermap.org/data/2.5/${type}?q=${city}&units=metric&appid=${key}`
         await axios.get(url).then(currentWeather)  
+    } else if(type == 'onecall') {
+        await axios.get(url).then(getCityGeoLocation)
     }
 }
+
+const getCityGeoLocation = async (response) => {
+    let latitude = response.data.coord.lat
+    let longitude = response.data.coord.lon
+    console.log(latitude, longitude)
+    let url = `https://api.openweathermap.org/data/2.5/onecall?&lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alert,current&units=metric&appid=${key}`
+    await axios.get(url).then(forecast)
+}
+
 
 const getLocationAPI = async (type, latitude, longitude) => {
+    let url = `https://api.openweathermap.org/data/2.5/${type}?&lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alert,current&units=metric&appid=${key}`
     if(type == 'weather') {
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`
         await axios.get(url).then(currentWeather) 
+    } else if(type == 'onecall') {
+        console.log('testing forecast')
+        console.log(url)
+        await axios.get(url).then(forecast)
     }
 }
-
 
 const defaultCity = (event) => {
     let city = 'Lansing'
+
     getAPICity('weather', city)   
+    getAPICity('onecall', city)   
 }
 
 defaultCity()
@@ -88,22 +131,60 @@ searchForm.addEventListener('submit', searchByName);
 
 //Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
 
-const convertTemperature = (event) => {
-    event.preventDefault()
-    let currentTemp = document.querySelector('#current_temp')
-    let convert = document.querySelector('#convert')
-
-    if(convert.innerHTML === '°C') {
-        currentTemp.innerHTML = Math.round((currentTemp.innerHTML * 1.8) + 32)
-        convert.innerHTML = '°F'
-    } else {
-        currentTemp.innerHTML = Math.round((currentTemp.innerHTML - 32) * .55)
-        convert.innerHTML = '°C'
+const tempConver = (temp, temp2, convert) => {
+    console.log(convert)
+    convert.trim()
+    
+    if(convert == 'C') {
+        temp = Math.round((temp * 1.8) + 32)
+        temp2 = Math.round((temp2 * 1.8) + 32)
+        console.log(temp2)
+        convert = 'F'
+    } else if(convert == 'F') {
+        temp = Math.round((temp - 32) * .55)
+        temp2 = Math.round((temp2 - 32) * .55)
+        convert = 'C'
+    }
+    return {
+        temp: temp,
+        temp2: temp2,
+        convert: convert
     }
 }
 
+const dispalyConvertions = (event) => {
+    event.preventDefault()
+    let currentTemp = document.querySelector('#current_temp')
+    let convert = document.querySelector('#convert')
+    let convertions = tempConver(currentTemp.innerHTML,1 , convert.innerHTML)
+    currentTemp.innerHTML = convertions.temp
+    convert.innerHTML =  convertions.convert
+    let daily = document.querySelectorAll('.daily')
+    daily.forEach(day => {
+        
+       
+        console.log(day)
+        let min = day.querySelector('.day_temp .min')
+        let max = day.querySelector('.day_temp .max')
+        convert =day.querySelector('.day_temp .convert')
+        console.log(convert)
+        convertions = tempConver(min.innerHTML, max.innerHTML, convert.innerHTML)
+        console.log(convertions)
+        min.innerHTML = convertions.temp;
+        max.innerHTML = convertions.temp2;
+        convert.innerHTML = convertions.convert
+
+    })
+
+
+}
+
+
+
+
+
 let convert = document.querySelector('#convert')
-convert.addEventListener('click', convertTemperature)
+convert.addEventListener('click', dispalyConvertions)
 
 const getGeoData = (event) => {
     event.preventDefault()
@@ -120,9 +201,11 @@ const sendGeoData = (position) => {
     if(latitude != '' && longitude != '')
     {
         getLocationAPI('weather', latitude, longitude)
+        getLocationAPI('onecall', latitude, longitude)
+        
     }       
 }
 
 let currentLocation = document.querySelector('#get_current_location')
 
-currentLocation.addEventListener('click', (getGeoData))
+currentLocation.addEventListener('click', getGeoData)
